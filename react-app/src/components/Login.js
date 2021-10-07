2e2b1faa44abd287def3bbecd7d170ba94828f2a
ldc1812 <locdcao@gmail.com>
2021-10-07 09:35:34 -0400

    Finishing up components

--- react-app/src/components/Login.js
+++ react-app/src/components/Login.js
@@ -9,7 +9,7 @@ import { useToast } from "@chakra-ui/react"
 
 const Login = () => {
     const socket = useContext(SocketContext)
-    const { name, setName, room, setRoom } = useContext(MainContext)
+    const { username, setName, room, setRoom } = useContext(MainContext)
     const history = useHistory()
     const toast = useToast()
     const { setUsers } = useContext(UsersContext)
@@ -24,7 +24,7 @@ const Login = () => {
 
     //Emits the login event and if successful redirects to chat and saves user data
     const handleClick = () => {
-        socket.emit('login', { name, room }, error => {
+        socket.emit('join', { username, room }, error => {
             if (error) {
                 console.log(error)
                 return toast({
@@ -52,7 +52,7 @@ const Login = () => {
         <Flex className='login' flexDirection='column' mb='8'>
             <Heading as="h1" size="4xl" textAlign='center' mb='8' fontFamily='DM Sans' fontWeight='600' letterSpacing='-2px'>Chat</Heading>
             <Flex className="form" gap='1rem' flexDirection={{ base: "column", md: "row" }}>
-                <Input variant='filled' mr={{ base: "0", md: "4" }} mb={{ base: "4", md: "0" }} type="text" placeholder='User Name' value={name} onChange={e => setName(e.target.value)} />
+                <Input variant='filled' mr={{ base: "0", md: "4" }} mb={{ base: "4", md: "0" }} type="text" placeholder='User Name' value={username} onChange={e => setName(e.target.value)} />
                 <Input variant='filled' mr={{ base: "0", md: "4" }} mb={{ base: "4", md: "0" }} type="text" placeholder='Room Name' value={room} onChange={e => setRoom(e.target.value)} />
                 <IconButton colorScheme='blue' isRound='true' icon={<RiArrowRightLine />} onClick={handleClick}></IconButton>
             </Flex>
